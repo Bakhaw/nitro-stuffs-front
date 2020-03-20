0e591e34244b3beb20dcd51b72e77dd5b172f97b
Ismail <aboualla.ismail@gmail.com>
2020-03-20 04:24:05 +0100

    CSGO-005: Add Swipeable Left Menu

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import { Link } from 'react-router-dom';
 
 import routes from './routes';
+import controllerIcon from '../../static/icons/controller.svg';
 
 function LeftMenu() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = open => () => {
+    setOpen(open);
+  };
+
   return (
-    <nav className='LeftMenu'>
-      <ul className='LeftMenu__links'>
-        {routes.map(({ img, to }, index) => (
-          <li className='LeftMenu__links__link' key={index}>
-            <Link to={to}>
-              <img
-                alt={img}
-                className='LeftMenu__links__img'
-                src={require(`../../static/images/${img}/game-icon/${img}.png`)}
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <React.Fragment>
+      <button className='LeftMenu_button' onClick={toggleDrawer(true)}>
+        <img
+          className='LeftMenu_button__img'
+          alt='ControllerIcon'
+          src={controllerIcon}
+        />
+      </button>
+      <SwipeableDrawer
+        disableDiscovery
+        anchor='left'
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
+        <div className='LeftMenu'>
+          <ul className='LeftMenu__links'>
+            {routes.map(({ img, to }, index) => (
+              <li className='LeftMenu__links__link' key={index}>
+                <Link onClick={toggleDrawer(false)} to={to}>
+                  <img
+                    alt={img}
+                    className='LeftMenu__links__img'
+                    src={require(`../../static/images/${img}/game-icon/${img}.png`)}
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </SwipeableDrawer>
+    </React.Fragment>
   );
 }
 
