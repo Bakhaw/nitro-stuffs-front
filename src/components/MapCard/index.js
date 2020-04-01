08e67b63be4059242b0eb9b572d0150dc8f7194c
Saoobi <aboualla.ismail@gmail.com>
2020-04-01 03:30:25 +0200

    [CSGO-004] Add stuff Filter button
    [CSGO-004]: minor fixes
    
    proptypes changes

--- src/components/MapCard/index.js
+++ src/components/MapCard/index.js
@@ -9,7 +9,7 @@ function MapCard({ map, match }) {
   const [opacity, setOpacity] = useState(0.5);
 
   const backgroundURL = require(`../../static/images/${game}/maps-previews/${key}.jpg`);
-  const backgroundImage = `linear-gradient(rgb(0, 0, 0) 0, rgba(0, 0, 0, ${opacity}) 0), url(${backgroundURL})`;
+  const backgroundImage = `url(${backgroundURL})`;
 
   function onCardHover() {
     const newOpacity = opacity === 0.5 ? 0.1 : 0.5;
