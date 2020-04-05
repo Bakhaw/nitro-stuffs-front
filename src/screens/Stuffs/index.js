61df72426e3d81c87ce52f2f98049a7e602fb448
Faïssal <faissal.hattou@gmail.com>
2020-04-06 00:33:08 +0200

    [CSGO-009] Add Back Button :art:

--- src/screens/Stuffs/index.js
+++ src/screens/Stuffs/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import BackButton from '../../components/BackButton';
 import StuffList from '../../components/StuffList';
 
 function Stuffs() {
-  return <StuffList />;
+  return (
+    <div className='Stuffs'>
+      <BackButton />
+      <StuffList />
+    </div>
+  );
 }
 
 export default Stuffs;
