58dceebb8c40058e85c2f5a0b0ce61514385536b
Hattou Faïssal <faissal.hattou@gmail.com>
2020-04-11 01:13:58 +0200

    [CSGO-014] Fixed undefined stuff error

--- src/API/csgoAPI/stuffs/stuffList.js
+++ src/API/csgoAPI/stuffs/stuffList.js
@@ -4,33 +4,289 @@ const stuffList = {
       id: '9avd46ed',
       title: 'Connector from T Spawn',
       type: 'flashbang',
-      videoId: 'FqDYqI9o2YI' // youtube video ID
+      videoId: 'FqDYqI9o2YI', // youtube video ID
     },
     {
       id: '9av22d4ed',
       title: 'Connector from T Spawn',
       type: 'molotov',
-      videoId: 'FqDYqI9o2YI' // youtube video ID
+      videoId: 'FqDYqI9o2YI', // youtube video ID
     },
     {
       id: '9avd4e3d',
       title: 'Connector from T Spawn',
       type: 'smoke',
-      videoId: 'FqDYqI9o2YI' // youtube video ID
+      videoId: 'FqDYqI9o2YI', // youtube video ID
     },
     {
       id: '9av1d4ed',
       title: 'Connector from T Spawn',
       type: 'smoke',
-      videoId: 'FqDYqI9o2YI' // youtube video ID
+      videoId: 'FqDYqI9o2YI', // youtube video ID
     },
     {
       id: '9avd2314ed',
       title: 'Connector from T Spawn',
       type: 'smoke',
-      videoId: 'FqDYqI9o2YI' // youtube video ID
-    }
-  ]
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_cobblestone: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_dust2: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_inferno: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_mirage: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_nuke: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_overpass: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_train: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
+  de_vertigo: [
+    {
+      id: '9avd46ed',
+      title: 'Connector from T Spawn',
+      type: 'flashbang',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av22d4ed',
+      title: 'Connector from T Spawn',
+      type: 'molotov',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd4e3d',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9av1d4ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+    {
+      id: '9avd2314ed',
+      title: 'Connector from T Spawn',
+      type: 'smoke',
+      videoId: 'FqDYqI9o2YI', // youtube video ID
+    },
+  ],
 };
 
 export default stuffList;
