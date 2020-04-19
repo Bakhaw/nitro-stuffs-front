0a312cd42db2fa32ebec778c2ae82f30a98d1438
Hattou Faïssal <faissal.hattou@gmail.com>
2020-04-19 09:01:13 +0200

    [VALORANT-001] Add Valorant maps

--- src/API/methods/maps/index.test.js
+++ src/API/methods/maps/index.test.js
@@ -1,15 +1,28 @@
 import { getMapByKey, getMapList } from '.';
 
 import csgoMaps from '../../static/maps/csgoMaps';
+import valorantMaps from '../../static/maps/valorantMaps';
 
-test('should return a mapList', () => {
+test('should return a CS:GO mapList', () => {
   const result = csgoMaps;
 
   expect(getMapList('csgo')).toEqual(result);
 });
 
-test('should return a map', () => {
+test('should return a Valorant mapList', () => {
+  const result = valorantMaps;
+
+  expect(getMapList('valorant')).toEqual(result);
+});
+
+test('should return a CS:GO map', () => {
   const result = csgoMaps[0]; // "de_cache"
 
   expect(getMapByKey('csgo', 'de_cache')).toEqual(result);
+});
+
+test('should return a Valorant map', () => {
+  const result = valorantMaps[0]; // "bind"
+
+  expect(getMapByKey('valorant', 'bind')).toEqual(result);
 });
