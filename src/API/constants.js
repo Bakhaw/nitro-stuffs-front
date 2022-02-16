906c2bdc509b21042f60c84dd0d7c447ae2a6655
Hattou Faïssal <faissal.hattou@gmail.com>
2022-02-16 23:55:01 +0100

    Use production API_BASE_URL

--- src/API/constants.js
+++ src/API/constants.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://nitro-stuffs-api.herokuapp.com/api';
-export const API_BASE_URL = 'http://localhost:3001/api';
+export const API_BASE_URL = 'https://nitro-stuffs-api.herokuapp.com/api';
+// export const API_BASE_URL = 'http://localhost:3001/api';
